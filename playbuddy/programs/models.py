aa40ce8600bdfd94e6384aca284878be559ce7ca
Yothin M <m@yothinix.com>
2019-05-20 22:14:36 +0700

    Able to display now showing tag :rocket:

--- playbuddy/programs/models.py
+++ playbuddy/programs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import pendulum
+
 from channels.models import Channel
 from movies.models import Movie
 
@@ -18,6 +20,15 @@ class Program(models.Model):
     @property
     def channel_name(self):
         return self.channel.display_name
+
+    @property
+    def now_showing(self):
+        now = pendulum.now()
+
+        if now.date() == self.date:
+            return self.start_time < now.time() < self.end_time
+        else:
+            return False
 
     def __str__(self):
         return f'{self.movie.name} [{self.date}] {self.start_time} - {self.end_time}'
